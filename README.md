0ca7a42ce82f7a06f5a52e4fccb574222cbb7de1
Club Rocket Power <148204327+r0cketp0wer@users.noreply.github.com>
2023-12-16 19:44:44 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Trending-Repos-Tracker
+# Trending Repositories
+
 This repository automatically updates to showcase trending GitHub repositories.
+
+## Trending Repositories
+
+- [Repository 1](https://github.com/username/repository1): Description for repository 1
+- [Repository 2](https://github.com/username/repository2): Description for repository 2
+- [Repository 3](https://github.com/username/repository3): Description for repository 3
+
+...
---
+++ update_trending.py
@@ -0,0 +1,17 @@
+import requests
+import os
+
+def fetch_trending_repositories():
+    url = "https://api.github.com/search/repositories?q=created:>2021-01-01&sort=stars&order=desc"
+    response = requests.get(url)
+    return response.json().get("items", [])
+
+def update_readme(trending_repositories):
+    with open("README.md", "w") as readme_file:
+        readme_file.write("# Trending Repositories\n\n")
+        for repo in trending_repositories:
+            readme_file.write(f"- [{repo['name']}]({repo['html_url']}): {repo['description']}\n")
+
+if __name__ == "__main__":
+    trending_repositories = fetch_trending_repositories()
+    update_readme(trending_repositories)
